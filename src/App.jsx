c451dce12b5241397904bcfd1e0294efa6170b88
Bek <chndn8486@gmail.com>
2024-03-25 14:22:53 +0530

    vfv

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ const App = () => {
     <>
       <Router>
         <Routes>
-          <Route path='/Restraunt_Reservation/' element={<Home/>}/>
+          <Route path='/' element={<Home/>}/>
           <Route path='/success' element={<Success/>}/>
           <Route path='*' element={<NotFound/>}/>
         </Routes>
