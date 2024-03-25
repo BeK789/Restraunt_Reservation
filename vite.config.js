320165a569fb9fad266996954b40f2253a2d9fc2
Bek <chndn8486@gmail.com>
2024-03-25 13:22:01 +0530

    vfv

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/Restraunt_Reservation/',
   plugins: [react()],
+  base: "/Restraunt_Reservation/"
 })
